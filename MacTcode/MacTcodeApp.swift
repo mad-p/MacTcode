7308ed3360ee7f1ea37f628fef25ccd25bd8d09b
Kaoru Maeda <kaoru.maeda@gmail.com>
2024-05-25 14:21:55 +0900

    Initial Commit

---
+++ MacTcode/MacTcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacTcodeApp.swift
+//  MacTcode
+//
+//  Created by maeda on 2024/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MacTcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
