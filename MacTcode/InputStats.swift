298e2b729432ab14417efa98da37a17b8e6ccbf6
Kaoru Maeda <kaoru.maeda@gmail.com>
2025-10-19 18:01:07 +0900

    setSignalHandlers as static func

--- MacTcode/InputStats.swift
+++ MacTcode/InputStats.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+var termPipe: [Int32] = [-1, -1]
+
 /// 入力統計情報を管理するシングルトンクラス
 class InputStats {
     static let shared = InputStats()
@@ -13,7 +15,7 @@ class InputStats {
     private let queue = DispatchQueue(label: "jp.mad-p.inputmethods.MacTcode.inputstats", attributes: .concurrent)
 
     private init() {
-        setupSignalHandlers()
+        InputStats.setupSignalHandlers()
     }
 
     /// 基本文字入力のカウントを増やす
@@ -21,6 +23,7 @@ class InputStats {
         queue.async(flags: .barrier) {
             self.basicCount += 1
             self.totalActionCount += 1
+            Log.i("basicCount = \(self.basicCount)")
         }
     }
 
@@ -105,26 +108,41 @@ class InputStats {
         }
     }
 
+    
     /// シグナルハンドラを設定
-    private func setupSignalHandlers() {
-        // SIG_IGNでデフォルトハンドリングを無視
-        signal(SIGINT, SIG_IGN)
-        signal(SIGTERM, SIG_IGN)
-
+    static func setupSignalHandlers() {
+        // パイプ（[読端, 書端]）
+        guard pipe(&termPipe) == 0 else { fatalError("pipe failed") }
+        
+        // C呼出規約、キャプチャなし
+        let sigtermHandler: @convention(c) (Int32) -> Void = { _ in
+            var one: UInt8 = 1
+            // write は async-signal-safe
+            _ = withUnsafePointer(to: &one) {
+                $0.withMemoryRebound(to: UInt8.self, capacity: 1) {
+                    write(termPipe[1], $0, 1)
+                }
+            }
+        }
+        
+        // シグナル登録
+        signal(SIGINT, sigtermHandler)
+        signal(SIGTERM, sigtermHandler)
+        
         // SIGINT用のDispatchSourceを作成
         let sigintSource = DispatchSource.makeSignalSource(signal: SIGINT, queue: .main)
-        sigintSource.setEventHandler { [weak self] in
+        sigintSource.setEventHandler {
             Log.i("★Received SIGINT, writing statistics...")
-            self?.writeStatsToFile()
+            shared.writeStatsToFile()
             exit(0)
         }
         sigintSource.resume()
-
+        
         // SIGTERM用のDispatchSourceを作成
         let sigtermSource = DispatchSource.makeSignalSource(signal: SIGTERM, queue: .main)
-        sigtermSource.setEventHandler { [weak self] in
+        sigtermSource.setEventHandler {
             Log.i("★Received SIGTERM, writing statistics...")
-            self?.writeStatsToFile()
+            shared.writeStatsToFile()
             exit(0)
         }
         sigtermSource.resume()
